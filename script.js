fb790f0a90de88e3d797e5f9cdb455c0f137f94b
junior77dark <sanoncheickfaycal7@gmail.com>
2024-12-19 08:08:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('welcomeButton').addEventListener('click', function() { 
     document.getElementById('welcomeMessage').textContent = 'Nous sommes ravis de vous accueillir sur le site dédié à l\'EPO. Explorez nos programmes et nos initiatives!'; 
     }); 
+<!-- Ajout après les éléments existants dans le corps du document --> 
+<p id="dynamicContent">Ceci est un contenu dynamique qui sera changé.</p> 
